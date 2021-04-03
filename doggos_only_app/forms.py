7fb9b6ba5004d27c91b390b00fd18eebd2237509
jpchato <jpchato@gmail.com>
2021-04-02 18:03:51 -0700

    added form for model

---
+++ doggos_only_app/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import *
+from django.forms import ModelForm
+
+class DogForm(forms.ModelForm):
+    class Meta:
+        model = Dog
+        fields = ['dog_name', 'dog_size']

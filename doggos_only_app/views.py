ee12208a194ad0b2ba669758734f1dd5ebaba447
jpchato <jpchato@gmail.com>
2021-04-02 18:44:14 -0700

    added second web page to display all dogs

--- doggos_only_app/views.py
+++ doggos_only_app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import *
 from .models import Dog
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Dab On Em One Time')
+def home(request):
+    if request.method == 'POST':
+        form = DogForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            plural_dog = Dog.objects.all()
+            return render(request, 'doggos_only_app/home.html', {'dog_images' : plural_dog})
+    else:
+        form = DogForm()
+    return render(request, 'doggos_only_app/home.html', {'form' : form})
+
+def display_images(request):
+    if request.method == 'GET':
+        plural_dog = Dog.objects.all()
+        return render(request, 'doggos_only_app/dog_images.html', {'dog_images' : plural_dog})

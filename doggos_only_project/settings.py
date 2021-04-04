769196776e6fbf8e13164997ebed9ccd3fb7bb50
jpchato <jpchato@gmail.com>
2021-04-03 17:04:39 -0700

    added file to static folder

--- doggos_only_project/settings.py
+++ doggos_only_project/settings.py
@@ -124,6 +124,7 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'doggos_only_app/static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 

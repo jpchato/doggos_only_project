d160a1a305d2995b7b7ef483d61a79b33df9519e
jpchato <jpchato@gmail.com>
2021-04-03 19:05:15 -0700

    updated settings

--- doggos_only_project/settings.py
+++ doggos_only_project/settings.py
@@ -14,18 +14,18 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8r8&vxas1ky&&fo(*#k@2eusq31y+ht*muy_!5+_)q9fnvn#+j'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['doggos-only.herokuapp.com', '127.0.0.1']
 
